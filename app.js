778cc49e3247c45c391686337a16c014438aef5c
aidankehoe <aidankehoe@gmail.com>
2022-10-04 17:57:53 +0100

    Updated

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-import * as THREE from '../../libs/three/three.module.js';
-import { OrbitControls } from '../../libs/three/jsm/OrbitControls.js';
+//import * as THREE from '../../libs/three/three.module.js';
+//import { OrbitControls } from '../../libs/three/jsm/OrbitControls.js';
+
+
 
 class App{
 	constructor(){
